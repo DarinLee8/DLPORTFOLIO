f124c896fbf17da4302ee504651f7108d96dbecd
DarinLee8 <lee.darin9@gmail.com>
2024-06-23 21:14:24 -0700

    adding new stuff to home and figuring out layout

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const contactButton = document.querySelector('#home .contact-button');
+    const links = document.querySelectorAll('#sidebar nav ul li a, #home .contact-button');
     
-    contactButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-        
-        targetSection.scrollIntoView({ behavior: 'smooth' });
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
     });
 });
