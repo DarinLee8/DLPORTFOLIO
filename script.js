24118efc6739ee630374951a92456e08630ea7c0
DarinLee8 <lee.darin9@gmail.com>
2024-06-24 18:28:05 -0700

    lots of changes

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     var navbar = document.querySelector('.navbar');
+    var aboutSection = document.getElementById('about'); // Get the About Me section
 
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 0) {
+    var aboutSectionOffset = aboutSection.offsetTop; // Offset position of the About Me section
+
+    function toggleNavbar() {
+        if (window.scrollY >= aboutSectionOffset) {
             navbar.classList.add('show');
         } else {
             navbar.classList.remove('show');
         }
-    });
+    }
+
+    // Initial check on page load
+    toggleNavbar();
+
+    // Listen for scroll events
+    window.addEventListener('scroll', toggleNavbar);
 });
